fb6f283d75a5271b58ccf9cc3d276e77137217cf
Shaikh Vakas Ahmed <97548328+Vakasahmed@users.noreply.github.com>
2022-11-25 07:41:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const enterDate = document.getElementById("enterDate");
 let realDate = document.getElementsByTagName("td");
 function date() {
-  for (let i = 0; i<realDate.length; i++) {
-    enterDate.value === realDate[i].innerText
-      ? realDate[i].style.backgroundColor === "white"
-        ? (realDate[i].style.backgroundColor = "green")
-        : (realDate[i].style.backgroundColor = "white")
-      : (realDate[i].style.backgroundColor = "white");
+  if (event.key === "Enter") {
+    for (let i = 0; i < realDate.length; i++) {
+      enterDate.value === realDate[i].innerText
+        ? realDate[i].style.backgroundColor === "white"
+          ? (realDate[i].style.backgroundColor = "green")
+          : (realDate[i].style.backgroundColor = "white")
+        : (realDate[i].style.backgroundColor = "white");
+    }
   }
+}
+
+function changeState() {
+  const select = document.getElementById("month");
+
+  let state = select.value;
+  document.getElementById("mon").innerText = state;
+}
+
+function changeYear() {
+  const currentYear = document.getElementById("year");
+
+  let state = currentYear.value;
+  document.getElementById("yr").innerText = state;
 }
