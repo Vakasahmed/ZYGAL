33e0f8d7420626726267331b15b890fcffe1694c
Shaikh Vakas Ahmed <97548328+Vakasahmed@users.noreply.github.com>
2022-11-25 07:56:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const enterDate = document.getElementById("enterDate");
 let realDate = document.getElementsByTagName("td");
 function date() {
+  for (let i = 0; i < realDate.length; i++) {
+    enterDate.value === realDate[i].innerText
+      ? realDate[i].style.backgroundColor === "white"
+        ? (realDate[i].style.backgroundColor = "green")
+        : (realDate[i].style.backgroundColor = "white")
+      : (realDate[i].style.backgroundColor = "white");
+  }
+}
+
+function dateTwo() {
   if (event.key === "Enter") {
     for (let i = 0; i < realDate.length; i++) {
       enterDate.value === realDate[i].innerText
